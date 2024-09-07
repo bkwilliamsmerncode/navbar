379a7a92700a0749f9833896306650fd37716248
Brian Williams <152632135+bkwilliamsmerncode@users.noreply.github.com>
2024-09-06 21:49:29 -0700

    first commit

---
+++ src/Navbar/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./Navbar.css";
+import { SiTheboringcompany } from "react-icons/si";
+import { IoMdMenu } from "react-icons/io";
+
+const Navbar = () => {
+  return (
+    <>
+      <nav id="main">
+        <div className="navLeft">
+          <div className="logo">
+            <SiTheboringcompany />
+          </div>
+        </div>
+        <div className="navRight">
+            <div className="pages"><p>Homepage</p></div>
+            <div className="pages"><p>About</p></div>
+            <div className="pages"><p>Contact</p></div>
+            <div className="pages"><p>Info</p></div>
+            <div className="pages"><p>Profile</p></div>
+            <div className="pages"><p>Logout</p></div>
+            <div className="pages"><IoMdMenu /></div>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
